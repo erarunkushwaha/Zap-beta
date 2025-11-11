9c22fc447cf4a1231a0f34274902bb51289f706d
erarunkushwaha <er.arunkushwaha@gmail.com>
2025-11-11 17:17:15 +0530

    webhook created

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,44 @@
 import express from "express";
 import cors from "cors";
 import { mylogger } from "./middleware/logger";
+import { Prisma, PrismaClient } from '@prisma/client'
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+
+
+const prisma = new PrismaClient()
 
 // Custom middleware to log HTTP requests
 app.use(mylogger);
 app.use(cors());
 app.use(express.json());
 
-app.post("/webhook/:userId/:zapId", (req, res) => {
-  console.log("body::", req.params);
-  res.status(200).json({ msg: "webhook created!" });
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const {userId, zapId} = req.params;
+  const {body} = req.body
+
+  // store in db a new trigger
+  await prisma.$transaction( async (tx:Prisma.TransactionClient) => {
+
+    const run = await tx.zapRun.create({
+      data:{
+        zapId:zapId,
+        metaData:body
+      }
+    })
+    await tx.zapRunOutbox.create({
+      data:{
+        zapRunid:run.id
+      }
+    });
+  })
+
+  res.status(200).json({ msg: "webhook received!" });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3002;
 app.listen(port, () => console.log(`Server running on port ${port}`));
